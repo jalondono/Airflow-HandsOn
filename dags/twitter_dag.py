e8f65896878a43b98c730c4771c69a94ff3c26f0
jalondono <juanlondono151776@hotmail.com>
2020-12-13 11:35:26 -0500

    [updated] addend cleaning task

--- dags/twitter_dag.py
+++ dags/twitter_dag.py
@@ -5,6 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 
 # this works because there is a PYTHONPATH ENV variable
 import fetching_tweet
+import cleaning_tweet
 
 default_args = {
     "start_date": datetime(2020, 1, 1),
@@ -13,3 +14,4 @@ default_args = {
 with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_args) as dag:
     waiting4tweets = FileSensor(task_id="waiting4tweets", fs_conn_id="fs_tweet", filepath="data.csv", poke_interval=5)
     fetching_tweets = PythonOperator(task_id="fetching_tweets", python_callable=fetching_tweet.main)
+    cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
