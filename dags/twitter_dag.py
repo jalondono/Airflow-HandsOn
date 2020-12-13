02f27afb4eda7ca3efb5505a2d3d9dd92033b1b5
jalondono <juanlondono151776@hotmail.com>
2020-12-13 12:12:54 -0500

    [updated] added Hive operator to load the data using hive

--- dags/twitter_dag.py
+++ dags/twitter_dag.py
@@ -3,6 +3,7 @@ from airflow.contrib.sensors.file_sensor import FileSensor
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.hive_operator import HiveOperator
 
 # this works because there is a PYTHONPATH ENV variable
 import fetching_tweet
@@ -18,3 +19,5 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
     cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
     storing_tweets = BashOperator(task_id="storing_tweets",
                                   bash_command="hadoop fs -put -f /tmp/data_cleaned.csv /tmp/")
+    loading_tweets = HiveOperator(task_id="loading_tweets",
+                                  hql="LOAD DATA INPATH '/tmp/data_cleaned.csv' INTO TABLE tweets")
