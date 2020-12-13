b053732448bce8bfd95209e32698dd756c88617d
jalondono <juanlondono151776@hotmail.com>
2020-12-13 12:02:19 -0500

    [updated] added bash operator to load data into hadoop

--- dags/twitter_dag.py
+++ dags/twitter_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 
 # this works because there is a PYTHONPATH ENV variable
 import fetching_tweet
@@ -15,3 +16,5 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
     waiting4tweets = FileSensor(task_id="waiting4tweets", fs_conn_id="fs_tweet", filepath="data.csv", poke_interval=5)
     fetching_tweets = PythonOperator(task_id="fetching_tweets", python_callable=fetching_tweet.main)
     cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
+    storing_tweets = BashOperator(task_id="storing_tweets",
+                                  bash_command="hadoop fs -put -f /tmp/data_cleaned.csv /tmp/")
