c67dc3caaf3ae3b3aca9ac4da1f6dabb9c4293ab
jalondono <juanlondono151776@hotmail.com>
2020-12-13 12:23:30 -0500

    [updated] added Dependencies between tasks

--- dags/twitter_dag.py
+++ dags/twitter_dag.py
@@ -13,7 +13,7 @@ default_args = {
     "start_date": datetime(2020, 1, 1),
     "owner": "jalondono_airflow"
 }
-with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_args) as dag:
+with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
     waiting4tweets = FileSensor(task_id="waiting4tweets", fs_conn_id="fs_tweet", filepath="data.csv", poke_interval=5)
     fetching_tweets = PythonOperator(task_id="fetching_tweets", python_callable=fetching_tweet.main)
     cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
@@ -21,3 +21,5 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
                                   bash_command="hadoop fs -put -f /tmp/data_cleaned.csv /tmp/")
     loading_tweets = HiveOperator(task_id="loading_tweets",
                                   hql="LOAD DATA INPATH '/tmp/data_cleaned.csv' INTO TABLE tweets")
+
+    waiting4tweets >> fetching_tweets >> cleaning_tweets >> storing_tweets >> loading_tweets
