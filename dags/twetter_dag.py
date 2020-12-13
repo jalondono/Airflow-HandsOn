2a7329365de2a53300647d479de8f2a0b0f7e276
jalondono <juanlondono151776@hotmail.com>
2020-12-13 11:19:37 -0500

    [updated] tweeter_dag - A Python task, It call a csv file and clean it

--- dags/twetter_dag.py
+++ dags/twetter_dag.py
@@ -1,10 +1,15 @@
 from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+
+# this works because there is a PYTHONPATH ENV variable
+import fetching_tweet
 
 default_args = {
     "start_date": datetime(2020, 1, 1),
     "owner": "jalondono_airflow"
 }
-with DAG(dag_id="twetter_dag", schedule_interval="@daily", default_args=default_args) as dag:
+with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_args) as dag:
     waiting4tweets = FileSensor(task_id="waiting4tweets", fs_conn_id="fs_tweet", filepath="data.csv", poke_interval=5)
+    fetching_tweets = PythonOperator(task_id="fetching_tweets", python_callable=fetching_tweet.main)
